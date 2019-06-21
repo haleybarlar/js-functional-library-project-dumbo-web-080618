0e866d84a507cce7c941a16ed4650d4dabd168e2
Haley Barlar <hbarlar1@gmail.com>
2019-06-21 06:54:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      for (let i = 0; i <= collection.length; i++) {
+        callback(collection[i])
+      }
     },
 
     map: function() {
@@ -15,7 +17,7 @@ fi = (function() {
     reduce: function() {
 
     },
-    
+
     functions: function() {
 
     },
